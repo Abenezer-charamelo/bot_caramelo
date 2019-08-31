b83db54e2b38166a14b3fea26e88c61efe7029d4
Abenezer-charamelo <54720729+Abenezer-charamelo@users.noreply.github.com>
2019-08-31 11:04:14 -0700

    Update sendmessage.py

--- sendmessage.py
+++ sendmessage.py
@@ -1,12 +1,12 @@
 import requests
 import json
 def send(to_,msg):
-    url="https://api.telegram.org/bot910587910:AAH7vmMms6iG76kEpHUUDGASefUFoRGOXmQ/sendmessage?chat_id={}&text={}".format(to_,msg)
+    url="https://api.telegram.org/bot******************************************/sendmessage?chat_id={}&text={}".format(to_,msg)
     requests.get(url)
 def chat_id_extractor(js):
     cht_id=js["result"][0]["message"]["from"]["id"]
     return cht_id
-"""token = "https://api.telegram.org/bot910587910:AAH7vmMms6iG76kEpHUUDGASefUFoRGOXmQ/getupdates?offset=246240940"
+"""token = "https://api.telegram.org/bot***********************************/getupdates?offset=246240940"
 r=requests.get(token)
 cus = json.loads(r.content)
 print(chat_id_extractor(cus))"""
